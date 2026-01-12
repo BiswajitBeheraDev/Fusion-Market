5b746603b9d3dd5cd069a931c13627644908e810
BiswajitBeheraDev <beherabiswajit344@gmail.com>
2026-01-12 17:29:09 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import { CartProvider } from "./context/cartcontext";
 import LayoutWrapper from "@/components/Layoutwrapper";
+import { Suspense } from "react";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -18,9 +19,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <CartProvider>
+          <Suspense fallback={<div>Loading...</div>}>
           <LayoutWrapper>
             {children}
           </LayoutWrapper>
+          </Suspense>
           <Toaster position="top-right" richColors closeButton duration={3000} />
         </CartProvider>
       </body>
