e57b5bf6b8817ffd4c14260b78e6f52f5f745ec9
BiswajitBeheraDev <beherabiswajit344@gmail.com>
2026-01-12 16:59:45 +0530

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,16 +6,12 @@ import { PrismaClient } from '@prisma/client';
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
-// Isko block scope se bahar declare karo taaki global access mile
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+export const prisma = global.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
