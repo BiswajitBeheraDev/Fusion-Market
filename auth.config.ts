584516578c619d7a45d87a139151ffb169c9a9f2
BiswajitBeheraDev <beherabiswajit344@gmail.com>
2026-01-13 19:56:44 +0530

    first

--- auth.config.ts
+++
@@ -1,9 +0,0 @@
-// auth.config.ts (optional)
-import type { NextAuthConfig } from "next-auth";
-
-export const authConfig = {
-  pages: {
-    signIn: "/login",
-  },
-  // callbacks, authorized, etc. can go here later
-} satisfies NextAuthConfig;
